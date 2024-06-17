3dc83d260885cd93ce5fb6c75276606ecaf58892
Samsonium <samsonovsa@kamaz.ru>
2024-06-17 16:41:02 +0300

    feat: implemented VGA driver though CRTC

--- src/drivers/vga.c
+++ src/drivers/vga.c
@@ -1,124 +1,117 @@
 #include "vga.h"
-#include "screen.h"
 #include "../cpu/ports.h"
 
-void draw_rectangle(int x, int y, int width, int height, u16 color);
-void vga_clear_screen();
-void vga_setpix(int x, int y, u16 color);
-
-u8 g_320x200x256[] = {
-    // MISC
-    0x63,
-
-    // SEQ
-    0x03, 0x01, 0x0F, 0x00, 0x0E,
-
-    // CRTC
-    0x5F, 0x4F, 0x50, 0x82, 0x54, 0x80, 0xBF, 0x1F,
-    0x00, 0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x9C, 0x0E, 0x8F, 0x28, 0x40, 0x96, 0xB9, 0xA3,
-
-    // GC
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x05, 0x0F,
-
-    // AC
-    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-    0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
-    0x41, 0x00, 0x0F, 0x00, 0x00};
-
-void vga_test()
+void write_registers(u8 *registers)
 {
-    kprint("Attempting to switch mode...", get_color(YELLOW, BLACK));
+    // misc
+    port_byte_out(VGA_MISC, *(registers++));
 
-    write_regs(g_320x200x256);
-    vga_clear_screen();
-
-    draw_rectangle(150, 10, 100, 50, GREEN);
-
-    for (int i = 0; i < 15; i++)
-    {
-        for (int j = 0; j < 100; j++)
-        {
-            vga_setpix(i, 50 + j, i);
-        }
-    }
-}
-
-void draw_rectangle(int x, int y, int width, int height, u16 color)
-{
-    for (int i = 0; i < width; i++)
-    {
-        for (int j = 0; j < height; j++)
-        {
-            vga_setpix(x + i, y + j, color);
-        }
-    }
-}
-
-void vga_clear_screen()
-{
-    draw_rectangle(0, 0, 320, 200, BLACK);
-}
-
-void vga_setpix(int x, int y, u16 color)
-{
-    u16 offset = x + 320 * y;
-    u16 *vga = (u16 *)VGA_ADDRESS;
-    vga[offset] = color;
-}
-
-void write_regs(u8 *regs)
-{
-    unsigned i;
-
-    // Write MISC reg
-    port_byte_out(VGA_MISC_WRITE, *regs);
-    regs++;
-
-    // Write SEQ regs
-    for (i = 0; i < VGA_NUM_SEQ_REGS; i++)
+    // seq
+    for (int i = 0; i < 5; i++)
     {
         port_byte_out(VGA_SEQ_INDEX, i);
-        port_byte_out(VGA_SEQ_DATA, *regs);
-        regs++;
+        port_byte_out(VGA_SEQ_DATA, *(registers++));
     }
 
-    // Unlock CRTC regs
+    // CRTC
     port_byte_out(VGA_CRTC_INDEX, 0x03);
     port_byte_out(VGA_CRTC_DATA, port_byte_in(VGA_CRTC_DATA) | 0x80);
     port_byte_out(VGA_CRTC_INDEX, 0x11);
-    port_byte_out(VGA_CRTC_DATA, port_byte_in(VGA_CRTC_DATA) & -0x80);
+    port_byte_out(VGA_CRTC_DATA, port_byte_in(VGA_CRTC_DATA) & ~0x80);
 
-    // Make sure CRTC is unlocked
-    regs[0x03] |= 0x80;
-    regs[0x11] &= -0x80;
+    registers[0x03] = registers[0x03] | 0x80;
+    registers[0x11] = registers[0x11] & ~0x80;
 
-    // Write CRTC regs
-    for (i = 0; i < VGA_NUM_CRTC_REGS; i++)
+    for (int i = 0; i < 25; i++)
     {
         port_byte_out(VGA_CRTC_INDEX, i);
-        port_byte_out(VGA_CRTC_DATA, *regs);
-        regs++;
+        port_byte_out(VGA_CRTC_DATA, *(registers++));
     }
 
-    // Write graphics controller regs
-    for (i = 0; i < VGA_NUM_GC_REGS; i++)
+    // GC
+    for (int i = 0; i < 9; i++)
     {
         port_byte_out(VGA_GC_INDEX, i);
-        port_byte_out(VGA_GC_DATA, *regs);
-        regs++;
+        port_byte_out(VGA_GC_DATA, *(registers++));
     }
 
-    // Write attribute controller regs
-    for (i = 0; i < VGA_NUM_AC_REGS; i++)
+    // AC
+    for (int i = 0; i < 21; i++)
     {
-        (void)port_byte_in(VGA_INSTAT_READ);
+        (void *)port_byte_in(VGA_AC_RESET);
         port_byte_out(VGA_AC_INDEX, i);
-        port_byte_out(VGA_AC_WRITE, *regs);
-        regs++;
+        port_byte_out(VGA_AC_WRITE, *(registers++));
     }
 
-    // Lock 16-color paletter and unblank display
-    (void)port_byte_in(VGA_INSTAT_READ);
+    port_byte_in(VGA_AC_READ);
     port_byte_out(VGA_AC_INDEX, 0x20);
+}
+
+int vga_supports_mode(u32 w, u32 h, u32 bbp)
+{
+    return w == 320 && h == 200 && bbp == 8;
+}
+
+int vga_set_mode(u32 w, u32 h, u32 dpp)
+{
+    if (vga_supports_mode(w, h, dpp) == 0)
+        return 1;
+
+    unsigned char g_320x200x256[] =
+        {
+            /* MISC */
+            0x63,
+            /* SEQ */
+            0x03, 0x01, 0x0F, 0x00, 0x0E,
+            /* CRTC */
+            0x5F, 0x4F, 0x50, 0x82, 0x54, 0x80, 0xBF, 0x1F,
+            0x00, 0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x9C, 0x0E, 0x8F, 0x28, 0x40, 0x96, 0xB9, 0xA3,
+            0xFF,
+            /* GC */
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x05, 0x0F,
+            0xFF,
+            /* AC */
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+            0x41, 0x00, 0x0F, 0x00, 0x00};
+
+    write_registers(g_320x200x256);
+    return 0;
+}
+
+u8 *get_framebuffer_segment()
+{
+    port_byte_out(VGA_GC_INDEX, 0x06);
+    u8 seg_num = ((port_byte_in(VGA_GC_DATA) >> 2) & 0x03);
+
+    switch (seg_num)
+    {
+    default:
+    case 0:
+        return (u8 *)0x00000;
+    case 1:
+        return (u8 *)0xA0000;
+    case 2:
+        return (u8 *)0xB0000;
+    case 3:
+        return (u8 *)0xB8000;
+    }
+}
+
+u8 vga_get_color_index(u8 r, u8 g, u8 b)
+{
+    if (r == 0x00 && g == 0x00 && b == 0xA8)
+        return 0x01;
+}
+
+void vga_putpixel(u32 x, u32 y, u8 color_index)
+{
+    u8 *pixelAddress = get_framebuffer_segment() + 320 * y + x;
+    *pixelAddress = color_index;
+}
+
+void vga_putpixel_c(u32 x, u32 y, u8 r, u8 g, u8 b)
+{
+    vga_putpixel(x, y, vga_get_color_index(r, g, b));
 }
--- src/drivers/vga.h
+++ src/drivers/vga.h
@@ -3,32 +3,26 @@
 
 #include "../cpu/types.h"
 
-#define VGA_ADDRESS 0xA0000
-
-void vga_test();
-void vga_clear_screen();
-void vga_setpix(int x, int y, u16 color);
-
-#define VGA_AC_INDEX 0x3C0
-#define VGA_AC_WRITE 0x3C0
-#define VGA_AC_READ 0x3C1
-#define VGA_INSTAT_READ 0x3DA
-#define VGA_MISC_WRITE 0x3C2
-#define VGA_MISC_READ 0x3CC
-
+#define VGA_MISC 0x3C2
 #define VGA_CRTC_INDEX 0x3D4
 #define VGA_CRTC_DATA 0x3D5
-#define VGA_GC_INDEX 0x3CE
-#define VGA_GC_DATA 0x3CF
 #define VGA_SEQ_INDEX 0x3C4
 #define VGA_SEQ_DATA 0x3C5
+#define VGA_GC_INDEX 0x3CE
+#define VGA_GC_DATA 0x3CF
+#define VGA_AC_INDEX 0x3C0
+#define VGA_AC_READ 0x3C1
+#define VGA_AC_WRITE 0x3C0
+#define VGA_AC_RESET 0x3DA
 
-#define VGA_NUM_AC_REGS 21
-#define VGA_NUM_CRTC_REGS 25
-#define VGA_NUM_GC_REGS 9
-#define VGA_NUM_SEQ_REGS 5
+int vga_supports_mode(u32 w, u32 h, u32 bbp);
+int vga_set_mode(u32 w, u32 h, u32 dpp);
 
-extern u8 g320x200x256[];
-void write_regs(u8 *regs);
+void write_registers(u8 *registers);
+u8 *get_framebuffer_segment();
+
+u8 vga_get_color_index(u8 r, u8 g, u8 b);
+void vga_putpixel(u32 x, u32 y, u8 color_index);
+void vga_putpixel_c(u32 x, u32 y, u8 r, u8 g, u8 b);
 
 #endif
