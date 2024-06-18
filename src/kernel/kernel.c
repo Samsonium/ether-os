f458452d5f86a47b4e9d29723fba35091a06d65c
Samsonium <tdesu@vk.com>
2024-06-18 20:02:03 +0300

    feat: add logo

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -6,6 +6,14 @@
 #include "../libc/mem.h"
 #include "cmd.h"
 
+const char *logo[] = {" ________  _  __                    ___    ______   ",
+					  "|_   __  |/ |[  |                 .'   `..' ____ \\  ",
+					  "  | |_ \\_`| |-| |--. .---. _ .--./  .-.  | (___ \\_| ",
+					  "  |  _| _ | | | .-. / /__\\[ `/'`\\| |   | |_.____`.  ",
+					  " _| |__/ || |,| | | | \\__.,| |   \\  `-'  | \\____) | ",
+					  "|________|\\__[___]|__'.__.[___]   `.___.' \\______.' ",
+					  "                                                    "};
+
 void kmain()
 {
 	isr_install();
@@ -13,7 +21,14 @@ void kmain()
 	init_paging();
 
 	clear_screen();
-	kprint("Welcome to EtherOS! Write something. Type \"END\" to halt the CPU", get_color(L_GREEN, BLACK));
+
+	for (int i = 0; i < 7; i++)
+	{
+		kprint(logo[i], get_color(L_GREEN, BLACK));
+		kprint("\n", 0x00);
+	}
+
+	kprint("Welcome to EtherOS! Write something. Type \"END\" to halt the CPU", get_color(WHITE, BLACK));
 	kprint("\n> ", get_color(L_CYAN, BLACK));
 }
 
