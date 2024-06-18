8b62593ef01c009fdb4d0f3b9f3792496eaa142a
Samsonium <tdesu@vk.com>
2024-06-19 02:40:19 +0300

    upd: change logo strings array to single string

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -6,13 +6,7 @@
 #include "../libc/mem.h"
 #include "cmd.h"
 
-const char *logo[] = {" ________  _  __                    ___    ______   ",
-					  "|_   __  |/ |[  |                 .'   `..' ____ \\  ",
-					  "  | |_ \\_`| |-| |--. .---. _ .--./  .-.  | (___ \\_| ",
-					  "  |  _| _ | | | .-. / /__\\[ `/'`\\| |   | |_.____`.  ",
-					  " _| |__/ || |,| | | | \\__.,| |   \\  `-'  | \\____) | ",
-					  "|________|\\__[___]|__'.__.[___]   `.___.' \\______.' ",
-					  "                                                    "};
+char logo[] = {" ________  _  __                    ___    ______   \n|_   __  |/ |[  |                 .'   `..' ____ \\  \n  | |_ \\_`| |-| |--. .---. _ .--./  .-.  | (___ \\_| \n  |  _| _ | | | .-. / /__\\[ `/'`\\| |   | |_.____`.  \n _| |__/ || |,| | | | \\__.,| |   \\  `-'  | \\____) | \n|________|\\__[___]|__'.__.[___]   `.___.' \\______.' \n,                                                    "};
 
 void kmain()
 {
@@ -22,11 +16,8 @@ void kmain()
 
 	clear_screen();
 
-	for (int i = 0; i < 7; i++)
-	{
-		kprint(logo[i], get_color(L_GREEN, BLACK));
-		kprint("\n", 0x00);
-	}
+	kprint(logo, get_color(L_GREEN, BLACK));
+	kprint("\n", get_color(BLACK, BLACK));
 
 	kprint("Welcome to EtherOS! Write something. Type \"END\" to halt the CPU", get_color(WHITE, BLACK));
 	kprint("\n> ", get_color(L_CYAN, BLACK));
