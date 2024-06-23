93a00a330e2d331cebbfe7a04f6ad8afbf566e6f
Samsonium <tdesu@vk.com>
2024-06-23 19:12:17 +0300

    feat: ports replaced with more complex io

---
+++ kernel/asm/io.h
@@ -0,0 +1,17 @@
+#ifndef IO_H
+#define IO_H
+
+#include <stdint.h>
+
+uint8_t byte_in(uint16_t port);
+uint16_t word_in(uint16_t port);
+
+void byte_out(uint16_t port, uint8_t value);
+void word_out(uint16_t port, uint16_t value);
+
+void io_interrupts_enable();
+void io_interrupts_disable();
+
+void io_wait();
+
+#endif
