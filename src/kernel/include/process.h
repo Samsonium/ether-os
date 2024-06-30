203638f438fc8357defb11e8443835781b72dc0b
Samsonium <tdesu@vk.com>
2024-06-30 18:29:21 +0300

    feat: implement process functions and handlers

--- src/kernel/include/process.h
+++ src/kernel/include/process.h
@@ -4,15 +4,18 @@
 #include <scheduler.h>
 #include <stdint.h>
 #include <interrupts.h>
+#include <stddef.h>
 
 struct process
 {
     uint64_t pid;
     void *stack_ptr;
     uint64_t state;
+
     uint64_t P4;
+    uint64_t brk;
+    uint64_t stack;
     QUEUE_SPOT(runQ);
-    uint8_t stack[];
 };
 
 struct process *process();
@@ -21,4 +24,6 @@ struct process *new_process(void (*function)(void));
 void yield();
 void start_scheduler();
 
-extern void switch_stack(void *old_ptr, void *new_ptr);
+void switch_stack(void *old_ptr, void *new_ptr);
+
+uint64_t procmm_brk(struct process *p, void *addr);
---
+++ src/kernel/proc/procmm.c
@@ -0,0 +1,52 @@
+#include <process.h>
+#include <memory.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <debug.h>
+#include <cpu.h>
+
+void procmm_init(struct process *p)
+{
+    p->P4 = new_P4();
+    p->brk = 0;
+    p->stack = KERNEL_OFFSET;
+}
+
+uint64_t procmm_brk(struct process *p, void *addr)
+{
+    while ((uint64_t)addr > p->brk)
+    {
+        vmm_set_page(p->P4, p->brk, pmm_alloc(), PAGE_USER | PAGE_WRITE | PAGE_PRESENT);
+        p->brk += PAGE_SIZE;
+    }
+
+    return p->brk;
+}
+
+registers *proc_pagefault(registers *r)
+{
+    if (!(r->rflags & (3 << 12)))
+    {
+        debug_error("Pagefault in kernel!\n");
+        debug("Interrupt %d, error code: %d\n", r->int_no, r->err_code);
+        debug_print_registers(r);
+        PANIC("Page fault in kernel\n");
+    }
+
+    uint64_t fault_addr = read_cr2();
+    uint64_t stack = process()->stack;
+    if (fault_addr + PAGE_SIZE >= stack)
+    {
+        if (stack - PAGE_SIZE <= process()->brk)
+            PANIC("Stack overflow in process %d\n", process()->pid);
+
+        stack -= PAGE_SIZE;
+        vmm_set_page(process()->P4, stack, pmm_alloc(), PAGE_USER | PAGE_WRITE | PAGE_PRESENT);
+        process()->stack = stack;
+
+        return r;
+    }
+
+    PANIC("Page fault in process\n");
+    return r;
+}
