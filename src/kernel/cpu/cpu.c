77773f4d0964738fad158abd5619bb51391dc286
Samsonium <tdesu@vk.com>
2024-07-02 23:32:19 +0300

    upd: add cpu allocation

--- src/kernel/cpu/cpu.c
+++ src/kernel/cpu/cpu.c
@@ -3,12 +3,17 @@
 #include <memory.h>
 #include <debug.h>
 
-void gdt_init(struct cpu *c);
+void gdt_init(cpu_t *c);
 
-struct cpu __seg_gs *cpu = 0;
+cpu_t __seg_gs *cpu = 0;
 
 void cpu_init()
 {
+    cpu_t *c = P2V(pmm_alloc());
+    c->cpu = c;
+    write_msr(KERNEL_GS_BASE, (uint64_t)c);
+    asm("swapgs");
+
     interrupt_init();
     gdt_init(cpu->cpu);
 }
