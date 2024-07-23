419a6e6009b393d368ed47f866f98b8061031c6b
Samsonium <tdesu@vk.com>
2024-07-23 19:06:43 +0300

    ref: remove old and not gold

--- src/kernel/include/keyboard.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <interrupts.h>
-
-#define INT_KB 33
-
-void keyboard_init();
-registers *handle_keyboard(registers *s);
--- src/kernel/include/vga.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define VGA_MEMORY P2V(0xB8000);
-
-void vga_init();
-void vga_write(char c);
-void vga_clear();
